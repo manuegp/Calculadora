2da97ea5c2ca61e61863661ef508be03cdf095c1
manuegp <manuel.mgp.2001@gmail.com>
2020-04-07 13:39:46 +0200

    Subir archivo a Github

---
+++ Calculadora/src/calculadora/Calculadora.java
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculadora;
+
+/**
+ *
+ * @author manue
+ */
+public class Calculadora {
+    private int num1; /*@see*/
+    private int num2;
+    
+    /**
+     *
+     * @param a
+     * @param b
+     */
+    public Calculadora (int a, int b){
+    num1=a;
+    num2=b;
+    } 
+    
+    /**
+     *
+     * @return
+     */
+    public int suma(){
+    int result =num1+num2;
+    return result;
+    }
+
+    /**
+     *
+     * @return
+     */
+    public int resta(){
+    int result =num1-num2;
+    return result;
+    }
+
+    /**
+     *
+     * @return
+     */
+    public int multiplicacion(){
+    int result =num1*num2;
+    return result;
+    }
+
+    /**
+     *
+     * @return
+     */
+    public int divide(){
+    int result =num1/num2;
+    return result;
+    }
+}
+
